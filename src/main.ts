b5476e784921d8eab1de23b3e1831d4afe041bb2
kjkandrea <kjkandrea@gmail.com>
2023-01-25 17:25:52 +0900

    feat: i18next 문자열 적용

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
 import './style.css';
+import i18next, {TFunction} from 'i18next';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+i18next
+  .init({
+    lng: 'ko',
+    debug: true,
+    resources: {
+      ko: {
+        translation: {
+          wild_pokemon_appeared: '야생의 {{pokemon}}(이)가 튀어나왔다!',
+          caught_a_pokemon: '신난다! {{pokemon}}(을)를 잡았다!',
+        },
+      },
+    },
+  })
+  .then(render);
+
+function render(t: TFunction) {
+  document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <p>${t('wild_pokemon_appeared', {pokemon: '리자몽'})}</p>
+    <p>${t('caught_a_pokemon', {pokemon: '리자몽'})}</p>
   </div>
 `;
+}
